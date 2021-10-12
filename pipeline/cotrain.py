Fix weighting by conf instead of prob

pipeline/cotrain.py
@@ -5,15 +5,14 @@ import math
 import datetime
 import logging
 import shutil
+import multiprocessing as mp
 from multiprocessing import Pool
 import numpy as np
 import torch
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence
 from torch.utils.data import DataLoader, Dataset
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
-from sklearn.metrics import f1_score
 import argparse
 from typing import Dict, List, Tuple
 from transformers import AutoTokenizer, AutoModel
@@ -43,6 +42,7 @@ def parse_args():
     parser.add_argument("--tune_hp", action="store_true")
     parser.add_argument("--cotrain_perfect", action="store_true")
     parser.add_argument("--cotrain_rate", default=0.05, type=float, help="Train with [0, 1]% of supervised labels")
+    parser.add_argument("--cotrain_patience", default=2, type=int)
     parser.add_argument("--seed", required=True, type=int, default=100)
 
     return parser.parse_args()
@@ -133,17 +133,19 @@ def compute_top_k_prob_mask(gen, dataset, algn_mask, k):
         
 def cotrain(src_gen, tgt_gen, src_train_dataset, tgt_train_dataset, src_algn_mask, tgt_algn_mask, rate) -> DataLoader:
     """ Augments Eraserdataset with self-labels. """
-    # with Pool(2) as p:
-    #     op = p.starmap(compute_top_k_prob_mask, [(src_gen, src_train_dataset, src_algn_mask, k), 
-    #                                              (tgt_gen, tgt_train_dataset, tgt_algn_mask, k)])
     src_total_tokens = torch.count_nonzero(src_algn_mask)
     src_size = math.ceil(rate * src_total_tokens)
-    src_top_k_prob_mask, src_scalar_metrics, src_r_mask, src_prob_mask = compute_top_k_prob_mask(src_gen, src_train_dataset, src_algn_mask, src_size)
-    print(src_scalar_metrics)
-
     tgt_total_tokens = torch.count_nonzero(tgt_algn_mask)
     tgt_size = math.ceil(rate * tgt_total_tokens)
+
+    # src_future = torch.jit.fork(compute_top_k_prob_mask, src_gen, src_train_dataset, src_algn_mask, src_size)
+    # tgt_future = torch.jit.fork(compute_top_k_prob_mask, tgt_gen, tgt_train_dataset, tgt_algn_mask, tgt_size)
+    # src_top_k_prob_mask, src_scalar_metrics, src_r_mask, src_prob_mask = torch.jit.wait(src_future)
+    # tgt_top_k_prob_mask, tgt_scalar_metrics, tgt_r_mask, tgt_prob_mask = torch.jit.wait(tgt_future)
+
+    src_top_k_prob_mask, src_scalar_metrics, src_r_mask, src_prob_mask = compute_top_k_prob_mask(src_gen, src_train_dataset, src_algn_mask, src_size)
     tgt_top_k_prob_mask, tgt_scalar_metrics, tgt_r_mask, tgt_prob_mask = compute_top_k_prob_mask(tgt_gen, tgt_train_dataset, tgt_algn_mask, tgt_size)
+    print(src_scalar_metrics)
     print(tgt_scalar_metrics)
 
 
@@ -197,8 +199,6 @@ def cotrain(src_gen, tgt_gen, src_train_dataset, tgt_train_dataset, src_algn_mas
         "tgt_top_k_p": tgt_p, "tgt_top_k_r": tgt_r, "tgt_top_k_f1": tgt_f1,
         "conflicting_labels": conflicting_labels
     }
-
-    print(overall_scalar_metrics)
 
     assert src_top_k_prob_mask.size(1) == len(src_train_dataset), f"Col i of prob mask corresponds to self labels for ith annotation. {len(src_top_k_prob_mask)} != {len(src_train_dataset)}"
     assert tgt_top_k_prob_mask.size(1) == len(tgt_train_dataset), f"Col i of prob mask corresponds to self labels for ith annotation. {len(tgt_top_k_prob_mask)} != {len(tgt_train_dataset)}"
@@ -278,13 +278,12 @@ def main():
     # best_val_target_metric = src_val_metrics["best_val_f1"] + src_val_metrics["best_val_tok_f1"] + tgt_val_metrics["best_val_f1"] + tgt_val_metrics["best_val_tok_f1"]
     best_val_target_metric = 0
     es_count = 0
+    cotrain_patience_count = 0
     growth_rate = args.cotrain_rate
     best_cotrain_rate = 0
     for t in range(epochs):
         logger.info(f"Epoch {t+1}\n-------------------------------")
         # augment train datasets with cotrain masks
-        # TODO: cotrain size is wrong. it should be by number of tokens.
-        # cur_cotrain_size = best_cotrain_size + math.ceil(cur_cotrain_rate * len(src_train_dataset) * avg_tokens)
         cur_cotrain_rate = best_cotrain_rate + growth_rate
         src_train_dataset, tgt_train_dataset, cotrain_scalar_metrics = cotrain(src_gen, tgt_gen, src_train_dataset, tgt_train_dataset, src_algn_mask, tgt_algn_mask, cur_cotrain_rate)
         src_train_dataloader = DataLoader(src_train_dataset, batch_size=config["train"]["batch_size"], shuffle=True, collate_fn=pad_collate)
@@ -325,13 +324,17 @@ def main():
             torch.save(tgt_enc.state_dict(), os.path.join(args.out_dir, "best_tgt_enc_weights.pth"))
 
             best_cotrain_rate = cur_cotrain_rate
+            cotrain_patience_count = 0
         else: 
             es_count += 1
             if es_count >= config["train"]["patience"]: 
                 logger.info("Early stopping!")
                 break
             
-            growth_rate /= 2
+            cotrain_patience_count += 1
+            if cotrain_patience_count >= args.cotrain_patience:
+                growth_rate /= 2
+                cotrain_patience_count = 0
             
     logger.info("Done training!")
     logger.info("Evaluating best model on test set")

pipeline/cotrain_utils.py
@@ -61,7 +61,8 @@ def tune_hp(config):
     # config["generator"]["selection_lambda"] = round(10 ** random.uniform(-4, -2), 5)
     # config["generator"]["continuity_lambda"] = round(10 ** random.uniform(-4, -2), 5)
     # config["train"]["lr"] = round(10 ** random.uniform(-4, -2), 5)
-    config["train"]["sup_pn"] = round(random.uniform(0.01, 0.05), 5)
+    # config["train"]["sup_pn"] = round(random.uniform(0.01, 0.05), 5)
+    config["train"]["rat_multp"] = round(random.uniform(2, 10), 5)
     
     return config
 
@@ -90,15 +91,19 @@ def train(dataloader, enc, gen, optimizer, args, device, config):
 
         # compute losses
         selection_cost, continuity_cost = gen.loss(mask)
-        if batch in label_batch_idx: mask_sup_loss = nn.BCELoss()(mask, r_pad)
-        elif not c_mask is None:
+        if batch in label_batch_idx: 
+            # NOTE: added rat multp! if works, should use it on rationale mask too
+            mask_sup_loss = config["train"]["rat_multp"] * nn.BCELoss()(mask, r_pad)
+        elif not c_mask[0] == None:
             # TODO: check if mask is correct
             # only apply BCE on nonzero values of cotrain mask
             mask_pred = mask[(c_mask + 1).nonzero(as_tuple=True)] # (L, bs)
-            mask_y_prob = c_mask[c_mask != -1] # (max_tokens, bs)
+            mask_y_prob = c_mask[c_mask != -1] # (max_tokens, bs)  # TODO: change to confidence
+            mask_y_conf = torch.abs(mask_y_prob - 0.5)
             mask_y = (mask_y_prob > 0.5).float()
-            mask_sup_loss = nn.BCELoss(mask_y_prob)(mask_pred, mask_y) 
+            mask_sup_loss = nn.BCELoss(mask_y_conf)(mask_pred, mask_y) 
             mask_sup_loss = torch.nan_to_num(mask_sup_loss)  # if no self-labels
+            print(f"mask_sup_loss: {mask_sup_loss}")
         else: mask_sup_loss = torch.tensor(0)
         obj_loss = nn.CrossEntropyLoss()(logit, l)
         loss = obj_loss + selection_cost + continuity_cost + mask_sup_loss